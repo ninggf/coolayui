ffc05a2a4017d10d37795afd3569060468bdae63
windywany <windywany@163.com>
2019-09-30 14:14:43 +0800

    add layui as submodule

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ const {
     watch
 } = require('gulp');
 
+const pkg = require('./package.json')
+const fs = require('fs')
 const sourcemap = require('gulp-sourcemaps')
 const identityMap = require('@gulp-sourcemaps/identity-map')
 const babel = require('gulp-babel')
@@ -22,27 +24,98 @@ const clean = require('gulp-rimraf')
 const uglify = require('gulp-uglify')
 const validate = require('gulp-jsvalidate')
 const notify = require('gulp-notify')
+const header = require('gulp-header')
 const open = require('gulp-open')
 
 const knownOptions = {
-    string: 'env',
-    default: {
-        env: process.env.NODE_ENV || 'dev'
+        string: 'env',
+        default: {
+            env: process.env.NODE_ENV || 'dev'
+        }
+    },
+    options = minimist(process.argv.slice(2), knownOptions)
+
+const cmt = '/** <%= pkg.name %>-v<%= pkg.version %> <%= pkg.license %> License By <%= pkg.homepage %> */\n <%= js %>',
+    note = [cmt, {
+        pkg: pkg,
+        js: ';'
+    }],
+    noteCss = [cmt, {
+        pkg: pkg,
+        js: ''
+    }],
+    layuiTasks = {
+        //压缩js模块
+        minjs(cb) {
+
+            //可指定模块压缩，eg：gulp minjs --mod layer,laytpl
+            let mod = options.mod ? function() {
+                    return '(' + options.mod.replace(/,/g, '|') + ')';
+                }() : '',
+                srcx = [
+                    'layui/src/**/*' + mod + '.js', '!layui/src/**/mobile/*.js', '!layui/src/lay/**/mobile.js', '!layui/src/lay/all.js', '!layui/src/lay/all-mobile.js'
+                ]
+
+            let gp = src(srcx)
+
+            if (options.env == 'pro')
+                gp = gp.pipe(uglify())
+
+            gp = gp.pipe(dest('./'))
+
+            if (options.watch)
+                gp.pipe(connect.reload())
+
+            cb()
+        },
+
+        //压缩css文件
+        mincss(cb) {
+            const srcx = [
+                'layui/src/css/**/*.css', '!layui/src/css/**/font.css'
+            ]
+
+            let gp = src(srcx)
+
+            if (options.env == 'pro')
+                gp = gp.pipe(cleancss())
+
+            gp = gp.pipe(dest('css'))
+
+            if (options.watch)
+                gp.pipe(connect.reload());
+
+            cb()
+        },
+
+        //复制iconfont文件
+        font(cb) {
+            src('layui/src/font/*')
+                .pipe(dest('font'))
+
+            cb()
+        },
+
+        //复制组件可能所需的非css和js资源
+        mv(cb) {
+            const srcx = ['layui/src/**/*.{png,jpg,gif,html,mp3,json}']
+
+            src(srcx).pipe(dest('./'))
+
+            cb()
+        }
     }
-};
 
-const options = minimist(process.argv.slice(2), knownOptions);
-
-const cleanTask = (cb) => {
-
-    src(['js/**/*.js', 'css/**/*.css', 'demo/**/*.html'], {
-        read: false
+const cleanTask = cb => {
+    src(['./lay/*', './css/*', './demo/*', './font/*', './images/*', './layui.js'], {
+        read: false,
+        allowEmpty: true
     }).pipe(clean())
 
     cb()
 }
 
-const buildCss = (cb) => {
+const buildCss = cb => {
     let gp = src(['src/less/*.less'])
 
     if (options.env != 'pro') {
@@ -66,7 +139,7 @@ const buildCss = (cb) => {
         gp = gp.pipe(sourcemap.write())
 
     if (options.env == 'pro')
-        gp = gp.pipe(cleancss())
+        gp = gp.pipe(cleancss()).pipe(header.apply(null, noteCss))
 
     gp = gp.pipe(dest('css'))
 
@@ -76,7 +149,7 @@ const buildCss = (cb) => {
     cb();
 }
 
-const buildJs = (cb) => {
+const buildJs = cb => {
     let gp = src(['src/js/*.js'])
 
     if (options.env != 'pro') {
@@ -100,17 +173,17 @@ const buildJs = (cb) => {
         gp = gp.pipe(uglify()).on('error', (e) => {
             notify.onError(e.message)
             console.error(e.message)
-        })
+        }).pipe(header.apply(null, note))
 
-    gp = gp.pipe(dest('js'))
+    gp = gp.pipe(dest('lay/plugins'))
 
     if (options.watch)
         gp.pipe(connect.reload());
 
     cb();
-};
+}
 
-const buildHtml = (cb) => {
+const buildHtml = cb => {
     let gp = src(['src/html/*.html', 'src/html/**/*.html']).pipe(preprocess({
         context: options
     })).on('error', e => {
@@ -121,9 +194,20 @@ const buildHtml = (cb) => {
         gp.pipe(connect.reload());
 
     cb()
-};
+}
 
-const watching = (cb) => {
+const mvAssets = cb => {
+    const srcx = ['./src/**/*.{png,jpg,gif,mp3,json,eot,svg,ttf,woff,woff2}']
+
+    let gp = src(srcx).pipe(dest('./'))
+
+    if (options.watch)
+        gp.pipe(connect.reload());
+
+    cb()
+}
+
+const watching = cb => {
     // start dev server
     connect.server({
         root: './',
@@ -142,19 +226,32 @@ const watching = (cb) => {
     watch(['src/html/**/*.html'], buildHtml)
     watch(['src/js/**/*.js'], buildJs)
     watch(['src/less/**/*.less'], buildCss)
+    watch(['src/**/*.{png,jpg,gif,html,mp3,json,eot,svg,ttf,woff,woff2}'], mvAssets)
 
+    if (fs.existsSync('layui/')) {
+        watch(['layui/src/**/*.js'], layuiTasks.minjs)
+        watch(['layui/src/**/*.css'], layuiTasks.mincss)
+    }
+
+    cb()
+}
+
+const buildLayui = fs.existsSync('layui/') ? parallel(layuiTasks.minjs, layuiTasks.mincss, layuiTasks.mv, layuiTasks.font) : cb => {
+    console.log('skip build layui')
     cb()
 }
 
 exports.clean = cleanTask
 
-exports.build = parallel(buildCss, buildJs, buildHtml);
+exports.build = parallel(buildCss, buildJs, buildHtml, mvAssets, buildLayui)
+
+exports.buildLayui = buildLayui
 
 exports.default = series(cb => {
     options.env = 'pro'
     cb()
 }, exports.build, cb => {
-    src(['js/**/**.dev.js', 'css/**/*.dev.css'], {
+    src(['lay/**/**.dev.js', 'css/**/*.dev.css'], {
         read: false
     }).pipe(clean())
 
